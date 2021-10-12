6059ac639997411e464bc940a53156b101d030a8
carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>
2021-10-11 20:21:27 -0500

    creacion consola
    Signed-off-by: carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>

---
+++ Hospital/Hospital.Consola/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hospital.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
