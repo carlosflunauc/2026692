90cdb7eb8eacca5ceb7dd05e7aec0abef69eda40
carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>
2021-10-11 20:57:02 -0500

    se creo la referencia de persistencia a dominio y se creo la carpeta entidades con la clase Persona
    Signed-off-by: carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>

---
+++ Hospital/Hospital.Dominio/Entidades/Persona.cs
@@ -0,0 +1,8 @@
+namespace Hospital.Dominio.Entidades
+{
+    public class Persona
+    {
+        public int Id {get; set;} //ID lo reconoce como Primary Key
+        public string Nombre {get; set;}
+    }
+}
